ef6d42eb01f2a46aaf23948ccbc381fdb56be95b
Mohamed AlMokadem <Mohamed.AlMokadem@vodafone.com>
2019-11-21 14:37:37 +0200

    removing unwanted code

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -4,18 +4,17 @@ var jwt = require("jsonwebtoken");
 var { User } = require("../models/user.model");
 var auth = require("../middleware/auth");
 const nodemailer = require("nodemailer");
-var mustache = require('mustache');
+var mustache = require("mustache");
 // const template = require("../utils/mailtemplate.html");
 // var nodemailer = require("../utils/nodemailler");
 
 // const upload = require('./upload');
 // const cors = require('cors');
 
-var fs = require('fs');
-const readXlsxFile = require('read-excel-file/node');
-var multer = require('multer');
+var fs = require("fs");
+const readXlsxFile = require("read-excel-file/node");
+var multer = require("multer");
 var upload = multer({ storage: multer.memoryStorage() });
-
 
 router.post("/register", async (req, res) => {
   var data = req.body;
@@ -30,18 +29,19 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/bulkRegister", async (req, res) => {
-
-  console.log('some on called bulkRegister api', req.body.Sheet1, req.body.Sheet1.length);
+  console.log(
+    "some on called bulkRegister api",
+    req.body.Sheet1,
+    req.body.Sheet1.length
+  );
   let data = req.body.Sheet1;
   // console.log('data is --> ', data);
   try {
-
     // await fs.writeFileSync("./Public/Excel/Users/newUsers.xlsx", req.file.buffer);
     var errorResult = [];
     // var rows = await readXlsxFile("./Public/Excel/Users/newUsers.xlsx");
 
     for (var i = 0; i < req.body.Sheet1.length; i++) {
-
       // console.log('inside for loop',req.body.Sheet1[i].userName);
       var uploadUser = {
         fullName: req.body.Sheet1[i].fullName,
@@ -52,10 +52,9 @@ router.post("/bulkRegister", async (req, res) => {
         department: req.body.Sheet1[i].department,
         company: req.body.Sheet1[i].company,
         enggaging: req.body.Sheet1[i].enggaging,
-        useful: req.body.Sheet1[i].useful,
-      }
-      console.log('uploadUser --> ', uploadUser);
-
+        useful: req.body.Sheet1[i].useful
+      };
+      console.log("uploadUser --> ", uploadUser);
     }
     // const result = Joi.validate(uploadUser, UserValidation);
     // if (result.error) {
@@ -65,12 +64,9 @@ router.post("/bulkRegister", async (req, res) => {
     if (errorResult.length > 0) {
       // console.log('errorResult --> ', errorResult);
       return res.status(400).send(errorResult);
-    }
-
-    else {
+    } else {
       // console.log('insdie else, data.lenght --> ', data);
       for (var i = 0; i < data.length; i++) {
-
         var user = await User.findOne({ email: data[i].email });
         if (user) {
           user.set({
@@ -79,19 +75,17 @@ router.post("/bulkRegister", async (req, res) => {
             staffId: data[i].staffId,
             firstName: data[i].firstName,
             lastName: data[i].lastName,
-            password: '123',
+            password: "123",
             department: data[i].department,
             company: data[i].company,
             enggaging: data[i].enggaging,
-            useful: data[i].useful,
+            useful: data[i].useful
           });
-          console.log('existed user before save--> ', user);
+          console.log("existed user before save--> ", user);
           await user.save();
-        }
-        else {
+        } else {
           var userByEmail = await User.findOne({ email: data[i].email });
           if (!userByEmail) {
-
             var newUser = new User({
               fullName: data[i].fullName,
               email: data[i].email,
@@ -99,32 +93,27 @@ router.post("/bulkRegister", async (req, res) => {
               firstName: data[i].firstName,
               lastName: data[i].lastName,
               department: data[i].department,
-              password: '123',
+              password: "123",
               company: data[i].company,
               enggaging: data[i].enggaging,
-              useful: data[i].useful,
+              useful: data[i].useful
             });
-            console.log('newUser after save--> ', newUser);
+            console.log("newUser after save--> ", newUser);
             const result = await newUser.save();
-            console.log('result of save', result);
+            console.log("result of save", result);
             if (result) {
-              console.log('nodemailer to --> ', result.email);
+              console.log("nodemailer to --> ", result.email);
 
               sendEmail(result);
-
-
             }
             console.log(result);
             // await newUser.save();
           }
         }
       }
-      return res.send({ message: 'Data inserted successfully.' });
+      return res.send({ message: "Data inserted successfully." });
     }
-
-  } catch (error) {
-
-  }
+  } catch (error) {}
 });
 
 router.post("/login", async (req, res) => {
@@ -138,10 +127,7 @@ router.post("/login", async (req, res) => {
 
     if (user) {
       return res.json({
-        token: jwt.sign(
-          { email: user.email, _id: user._id },
-          "key"
-        )
+        token: jwt.sign({ email: user.email, _id: user._id }, "key")
       });
     } else {
       return res.status(500).send("Invalid NT or Password.");
@@ -155,106 +141,87 @@ router.get("/authData", auth, async (req, res) => {
   res.json({ message: "Authenication", data: req.user });
 });
 
-
 router.get("/users", async (req, res) => {
-
-  let users = await User.find( function(error , response){
-    
+  let users = await User.find(function(error, response) {
     if (error) {
       res.send(error);
-    }else{
-      console.log('response', response);
+    } else {
+      console.log("response", response);
       res.send(response);
     }
   });
-
 });
 
-router.post("/sectionupdate", async (req,res) =>{
-
-  // console.log('inside sectionupdate api', req);
-  // var reqUser = new User({
-  //   id: req.body._id,
-  //   lastSection: req.body.lastSection
-  // });
-
-  // console.log('reqUser is -->', reqUser.id);
-  // console.log('req._id is  -->', req.body._id);
-
-  let user = await User.findOne({_id :req.body._id}, function (error, response) {
+router.post("/sectionupdate", async (req, res) => {
+  let user = await User.findOne({ _id: req.body._id }, function(
+    error,
+    response
+  ) {
     if (error) {
       // console.log(error);
       res.send(error);
-    }else if (response) {
-      // console.log('response is -->',response); 
     }
   });
-  console.log('let user is -->', user);
 
   if (!user) {
-    res.status(404).send('No such user found');
-  }else{
+    res.status(404).send("No such user found");
+  } else {
     user.lastSection = req.body.lastSection;
 
     // const result = await user.save();
 
-    const newUser = await User.findOneAndUpdate({ _id:req.body._id}, {$set: { lastSection: user.lastSection }}, {upsert:true}, function(err, doc){
-      if (err){
-
-         return res.send(500, { error: err });
-      }else{
-        return res.status(200).send("succesfully saved");
+    const newUser = await User.findOneAndUpdate(
+      { _id: req.body._id },
+      { $set: { lastSection: user.lastSection } },
+      { upsert: true },
+      function(err, doc) {
+        if (err) {
+          return res.send(500, { error: err });
+        } else {
+          return res.status(200).send("succesfully saved");
+        }
       }
-  });
-  // console.log('success newuser is --> ', newUser);
-
-
-    // console.log('result is -->', result);
-    // res.status(200).send(result);
-
+    );
   }
 });
 
-
 function sendEmail(user) {
-  fs.readFile('./utils/passwordTemplate.html', 'utf8',
-    function (err, mailTemplate) {
-      if (err) throw err;
-      console.log('inside sendEmail mailTo is-->', user);
-      // TODO: make the mail template ready
-      let actualMail = mustache.render(mailTemplate, user);
+  fs.readFile("./utils/passwordTemplate.html", "utf8", function(
+    err,
+    mailTemplate
+  ) {
+    if (err) throw err;
+    console.log("inside sendEmail mailTo is-->", user);
+    // TODO: make the mail template ready
+    let actualMail = mustache.render(mailTemplate, user);
 
-      let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        requireTLS: true,
-        auth: {
-          user: 'm.osamaalmokadem@gmail.com',
-          pass: 'loveyoubaby'
-        }
-      });
-
-      let mailOptions = {
-        from: 'm.osamaalmokadem@gmail.com',
-        to: user.email,
-        subject: 'Welcome ' +user.fullName+' to Vodafone',
-        html: actualMail
-      };
-
-      transporter.sendMail(mailOptions,
-        function (error, info) {
-          if (error) {
-            throw error
-          } else {
-            console.log('Email sent: ' + info.response);
-          }
-        });
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      requireTLS: true,
+      auth: {
+        user: "m.osamaalmokadem@gmail.com",
+        pass: "loveyoubaby"
+      }
     });
 
+    let mailOptions = {
+      from: "m.osamaalmokadem@gmail.com",
+      to: user.email,
+      subject: "Welcome " + user.fullName + " to Vodafone",
+      html: actualMail
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+        throw error;
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+  });
 }
-
-
 
 module.exports = router;
